a65f9fb3d73c5a4f1a208822f1bd657641e5d208
AlexGo2 <alex.oliveira29@hotmail.com>
2022-05-02 22:36:47 -0300

    updates

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
-const app = express(app);
+const res = require('express/lib/response');
+const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res)=>{ 
+app.get(port, (req, res)=>{ 
     res.send('Heroku, Online!');
 });
 
 app.listen(port, ()=>{ 
-    app.send('Porta Online!');
+    console.log('Porta Online!');
 });
